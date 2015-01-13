7e5480de8a25bfb8bfc9b924c33b77b9fb4b28e8
f03lipe <pires.a.felipe@gmail.com>
2015-01-13 06:37:56 -0200

    Homework 1

--- CIS 194/hw1.hs
+++ CIS 194/hw1.hs
@@ -12,14 +12,25 @@ toDigitsRev n = reverse (toDigits n)
 -- Exercise 2
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther (a:b:c) = [2*a,b] ++ doubleEveryOther(c)
-doubleEveryOther (x:[]) = [2*x]
+doubleEveryOther (a:b:c) = (if (mod (length c) 2) == 0 then [2*a, b]
+	else [a, 2*b]) ++ doubleEveryOther(c)
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther [] = []
 
 -- Exercise 3
 sumDigits :: [Integer] -> Integer
--- sumDigits n = sum n
 sumDigits [] = 0
-sumDigits (a:b) = a + sumDigits b
+sumDigits (a:b) = sum (toDigits a) + sumDigits b
 
 -- Exercise 4
+validate :: Integer -> Bool
+validate n = if (sumDigits (doubleEveryOther (toDigits n)) `mod` 10) == 0
+	then True
+	else False
 
+-- Exercise 5
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
